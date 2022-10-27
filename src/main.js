871c0e221399e7775b1bf97833513897420d32ef
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-10-27 14:27:38 +0100

    fix: use table as a default output

--- src/main.js
+++ src/main.js
@@ -9,9 +9,10 @@ module.exports = async (options) => {
   const result = await audit(options.url);
   const gradeInput = grades.findIndex((o) => o === options.grade);
   const gradeOutput = grades.findIndex((o) => o === result.grade);
-  if (options.output === "table") reportResult(result, options);
   if (options.output === "csv") reportCsvResult(result, options);
   if (options.output === "json") reportJsonResult(result, options);
+  if (options.output === "table" || !options.output) reportResult(result, options);
+
   if (gradeInput !== -1 && gradeOutput > gradeInput) {
     console.error(
       `Your grade is ${gradeOutput}, but should be below ${gradeInput}`
