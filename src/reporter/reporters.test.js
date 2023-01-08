2e96f66ebcfc88cec046e88d72c9093ca84b86af
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-01-08 19:59:03 +0100

    fix: rewrite unit test

--- src/reporter/reporters.test.js
+++ src/reporter/reporters.test.js
@@ -12,15 +12,42 @@ test("should report with CSV", () => {
   const consoleLogMock = jest.spyOn(global.console, "log").mockImplementation();
   csv(sampleAudit, { visits: 2000 });
   const result = consoleLogMock.mock.calls.join("\n");
-  expect(result).toMatchSnapshot();
+  console.log(result)
+  expect(result).toEqual(`Métrique,Valeur,Informations complémentaires
+EcoIndex,56/100,
+Note,C,
+GES,1.88eqCO2,Pour un total de 2000 visites par mois, ceci correspond à 42km en voiture (Peugeot 208 5P 1.6 BlueHDi FAP (75ch) BVM5)
+Eau,2.82cl,Pour un total de 2000 visites par mois, ceci correspond à 1 douche`);
   consoleLogMock.mockRestore();
 });
 
 test("should report with JSON", () => {
+  const expected = JSON.stringify([
+    {
+      "label": "EcoIndex",
+      "value": 56
+    },
+    {
+      "label": "Note",
+      "value": "C"
+    },
+    {
+      "label": "GES",
+      "value": 1.88,
+      "unit": "gCO2e",
+      "comment": "Pour un total de 2000 visites par mois, ceci correspond à 42km en voiture (Peugeot 208 5P 1.6 BlueHDi FAP (75ch) BVM5)"
+    },
+    {
+      "label": "Eau",
+      "value": 2.82,
+      "unit": "cl",
+      "comment": "Pour un total de 2000 visites par mois, ceci correspond à 1 douche"
+    }
+  ]);
+
   const consoleLogMock = jest.spyOn(global.console, "log").mockImplementation();
   json(sampleAudit, { visits: 2000 });
   const result = consoleLogMock.mock.calls.join("\n");
-  expect(result).toMatchSnapshot();
+  expect(JSON.stringify(JSON.parse(result))).toEqual(expected);
   consoleLogMock.mockRestore();
 });
-
