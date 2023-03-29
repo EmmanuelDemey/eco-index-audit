11c98ca065955fc759103896e7908e807a5ca518
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-03-29 13:56:58 +0200

    fix: solve eslint issues

--- src/reporter/reporters.test.js
+++ src/reporter/reporters.test.js
@@ -112,58 +112,6 @@ test("should report with JSON", () => {
 });
 
 test("should report with Sonar", () => {
-  const expected = JSON.stringify({
-    score: 56,
-    grade: "C",
-    estimatation_co2: {
-      comment:
-        "Pour un total de 2000 visites par mois, ceci correspond à 42km en voiture (Peugeot 208 5P 1.6 BlueHDi FAP (75ch) BVM5)",
-      commentDetails: { numberOfVisit: 2000, value_km: 42, value: 42, unit: "km" },
-    },
-    estimatation_water: {
-      comment: "Pour un total de 2000 visites par mois, ceci correspond à 1 douche",
-      commentDetails: { numberOfVisit: 2000, value_shower: 1, value: 1, unit: "douches" },
-    },
-    pages: [
-      {
-        url: "url",
-        ecoIndex: 56,
-        grade: "C",
-        greenhouseGasesEmission: 1.88,
-        waterConsumption: 2.82,
-        metrics: [
-          {
-            name: "number_requests",
-            value: 16,
-            status: "info",
-            recommandation: "< 30 requests",
-          },
-          {
-            name: "page_size_kbytes",
-            value: 1200,
-            status: "warning",
-            recommandation: "< 1000kb",
-          },
-          {
-            name: "Page_complexity",
-            value: 3000,
-            status: "error",
-            recommandation: "Between 300 and 500 nodes",
-          },
-        ],
-        estimatation_co2: {
-          comment:
-            "Pour un total de 2000 visites par mois, ceci correspond à 42km en voiture (Peugeot 208 5P 1.6 BlueHDi FAP (75ch) BVM5)",
-          commentDetails: { numberOfVisit: 2000, value_km: 42, value: 42, unit: "km" },
-        },
-        estimatation_water: {
-          comment: "Pour un total de 2000 visites par mois, ceci correspond à 1 douche",
-          commentDetails: { numberOfVisit: 2000, value_shower: 1, value: 1, unit: "douches" },
-        },
-      },
-    ],
-  });
-
   const consoleLogMock = jest.spyOn(global.console, "log").mockImplementation();
   sonar(sampleAudit, { ecoIndex: 100, sonarFilePath: "sonarFilePath" });
   const result = consoleLogMock.mock.calls;
