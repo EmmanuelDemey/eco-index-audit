18b45dd8e81a24e0078ec64ce2531e6b1d9c3ff0
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-06-08 12:31:01 +0200

    feat: add new Sonar rules and bump to 4.5.0

--- src/reporter/sonar.js
+++ src/reporter/sonar.js
@@ -5,9 +5,10 @@ module.exports = (result, options) => {
     console.error("You should define the sonarFilePath property");
     process.exit(1);
   }
+  const issues = [];
 
   if (options.ecoIndex > result.ecoIndex) {
-    
+
     const sonar = {
       engineId: "eco-index",
       ruleId: "eco-index-below-threshold",
@@ -18,13 +19,41 @@ module.exports = (result, options) => {
         filePath: options.sonarFilePath,
       },
     };
+    issues.push(sonar);
+  }
 
-    if(options.outputPathDir){
-      fs.mkdirSync(options.outputPathDir, { recursive: true });
-      fs.writeFileSync(options.outputPathDir + "/" + (options.outputFileName ?? "report") + "-sonar.json", JSON.stringify({issues: [sonar]}));
+
+  const generateMetricMessage = (name, value, status, recommandation) => {
+    //`You ecoindex (${result.ecoIndex}) is below the configured threshold (${options.ecoIndex})`
+    if(name === "number_requests"){
+      return `The number of HTTP requests (${value}) is below the configured threshold (${recommandation})`
     }
+    else if(name === "page_size"){
+      return `The size of the page (${value}) is below the configured threshold (${recommandation})`
+    }
+    else if(name === "Page_complexity"){
+      return `The complexity of the page (${value}) is below the configured threshold (${recommandation})`
+    }
+  }
+  result.pages?.forEach(({ url, metrics }) => {
+    metrics?.forEach(({ name, value, status, recommandation }) => {
+      if(status === "warning" || status === "error"){
+        issues.push({
+          engineId: "eco-index",
+          ruleId: "eco-index-" + name.toLowerCase(),
+          severity: status === "warning" ? "MINOR" : "MAJOR",
+          type: "BUG",
+          primaryLocation: {
+            message: generateMetricMessage(name, value, status, recommandation) + ` (${url})` ,
+            filePath: options.sonarFilePath,
+          },
+        })
+      }
+    })
+  });
 
-    console.log(sonar);
-
+  if(options.outputPathDir && issues.length > 0){
+    fs.mkdirSync(options.outputPathDir, { recursive: true });
+    fs.writeFileSync(options.outputPathDir + "/" + (options.outputFileName ?? "report") + "-sonar.json", JSON.stringify({issues}));
   }
 };
